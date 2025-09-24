d293d2be0fc944dd4ea4942e2baade25a43b8fd4
TanyaHaidar <tfilippenko1@gmail.com>
2025-09-24 21:20:27 +0300

    ts5

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -4,7 +4,7 @@ import { Note, CreateNoteDTO } from "../types/note";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
+    Authorization: `Bearer ${import.meta.env.VITE_NOTEHUB_TOKEN}`,
   },
 });
 export interface FetchNotesParams {
